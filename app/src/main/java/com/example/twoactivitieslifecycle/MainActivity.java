e3c084580c5769bf471cb9d36fd5a9f0ef570a80
James Lamichhane <ljames19@tbc.edu.np>
2022-04-06 01:24:09 +0545

    Homework complete

--- app/src/main/java/com/example/twoactivitieslifecycle/MainActivity.java
+++ app/src/main/java/com/example/twoactivitieslifecycle/MainActivity.java
@@ -1,76 +1,32 @@
 package com.example.twoactivitieslifecycle;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import org.w3c.dom.Text;
-
-import java.util.HashMap;
-
 public class MainActivity extends AppCompatActivity {
 
-    public static final int TEXT_REQUEST = 1;
-    private ShopList items = new ShopList();
-    public MainActivity() {
-    }
-
+    private int mCount = 0;
+    private TextView mShowCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mShowCount=(TextView) findViewById(R.id.number);
 
-        Intent intent = getIntent();
-        if ((savedInstanceState != null)&& (savedInstanceState.getSerializable("list")!= null)){
-            HashMap<String, Integer> l = (HashMap<String, Integer>)savedInstanceState.getSerializable("list");
-            TextView tv = findViewById(R.id.textView);
-            tv.setText("");
-            for (String k : l.keySet()) {
-                String s = l.get(k).toString() + " " + k + "\n";
-                tv.setText(tv.getText() + s);
-                for(int i = 0; i<l.get(k); i++){
-                    items.addItem(k);}
-            }
+        if (savedInstanceState != null) {
+            mShowCount.setText(savedInstanceState.getString("reply_text"));
         }
     }
-
-    public void launchSecondActivity(View view) {
-
-        Intent intent = new Intent(this, SecondActivity.class);
-
-        startActivityForResult(intent, TEXT_REQUEST);
-    }
-
     @Override
-    protected void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putSerializable("list", items.getItems());
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("reply_text", mShowCount.getText().toString());
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String item = data.getStringExtra(SecondActivity.EXTRA_MESSAGE);
-                items.addItem(item);
-            }
-            drawView();
-        }
-    }
-
-    public void drawView() {
-        HashMap<String, Integer> l = items.getItems();
-        TextView tv = findViewById(R.id.textView);
-        tv.setText("");
-        for (String k : l.keySet()) {
-            String s = l.get(k).toString() + " " + k + "\n";
-            tv.setText(tv.getText() + s);
-        }
+    public void increment(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
     }
 }
